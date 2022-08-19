e0e9e7e562e63abe4c879382bda50537148c9be0
Kevin Schaefer <kevinss.hello@gmail.com>
2022-08-18 17:51:37 -0700

    placeholders for site layout added

--- src/components/Layouts/index.js
+++ src/components/Layouts/index.js
@@ -1,11 +1,23 @@
-import './index.scss'
+import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar'
+import './index.scss'
 
 const Layout = () => {
   return (
-    <>
+    <div className="App">
       <Sidebar />
-    </>
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+
+        <span className="tags bottoms-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
   )
 }
 
