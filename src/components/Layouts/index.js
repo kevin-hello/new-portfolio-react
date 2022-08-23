dd358528dfce27b0fce73640714d40049bf19385
Kevin Schaefer <kevinss.hello@gmail.com>
2022-08-22 17:23:28 -0700

    dependencies updated, homepage completed

--- src/components/Layouts/index.js
+++ src/components/Layouts/index.js
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom'
-import Sidebar from '../Sidebar'
+import Sidebar from '../Sidebar/'
 import './index.scss'
 
 const Layout = () => {
@@ -10,8 +10,7 @@ const Layout = () => {
         <span className="tags top-tags">&lt;body&gt;</span>
 
         <Outlet />
-
-        <span className="tags bottoms-tags">
+        <span className="tags bottom-tags">
           &lt;/body&gt;
           <br />
           <span className="bottom-tag-html">&lt;/html&gt;</span>
